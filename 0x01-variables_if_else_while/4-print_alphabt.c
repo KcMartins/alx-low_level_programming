3d2ad681a457193e67496cdeeb806cd75e55b2a5
KcMartins <Kcmartinsikechukwu@yahoo.com>
2022-09-09 03:58:22 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase
+ * Print all the letters except q and e,
+ * followed by a new line.
+ * Returns: 0 (Success)
+ */
+int main(void)
+{
+	char mine = 'a';
+	while (mine <= 'z')
+	{
+		if (mine != 'e' && mine != 'q')
+		{
+			putchar(mine);
+		}
+		mine++;
+	}
+	putchar('\n');
+	return (0);
+}
