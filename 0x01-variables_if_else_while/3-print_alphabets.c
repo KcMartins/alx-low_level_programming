1081a2e89a7adb10d02912f7aa1797d7692cb113
KcMartins <Kcmartinsikechukwu@yahoo.com>
2022-09-09 04:16:43 -0700

    Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,11 +2,12 @@
 /**
  * main - prints the alphabet in lowercase, and then in uppercase,
  *  followed by a new line.
- *  Returns: 0 (Success)
+ *  Return: Always 0 (Success)
  */
 int main(void)
 {
 	char mine;
+
 	for (mine = 'a'; mine <= 'z'; mine++)
 	{
 		putchar(mine);
