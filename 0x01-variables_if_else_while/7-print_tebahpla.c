d8085098084798056a62538334307dbf8f001d1d
KcMartins <Kcmartinsikechukwu@yahoo.com>
2022-09-09 06:41:16 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints the lowercase alphabet in reverse,
+ * followed by a new line.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char mine;
+	for (mine = 'z'; mine >= 'a'; mine--)
+	{
+		putchar(mine);
+	}
+	putchar('\n');
+	return (0);
+}
