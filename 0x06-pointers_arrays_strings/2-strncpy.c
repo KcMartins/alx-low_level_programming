ecfc4f56fa46774627b172b31e3b6875c9c6af29
KcMartins <kcmartinsikechukwu@yahoo.com>
2022-09-22 01:23:19 -0700

    Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,8 +3,7 @@
  * _strncpy - copies a string
  * @dest: destination string
  * @src: source string
- * @n: number of bytes to copy
- * 
+ * @n: number of bytes to copy 
  * Return: pointer to the resulting string
  */
 char *_strncpy(char *dest, char *src, int n)
