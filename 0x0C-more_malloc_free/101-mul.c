359a754580fc764b9c0eeb073531b4cb6900a1c1
KcMartins <kcmartinsikechukwu@yahoo.com>
2022-10-06 08:55:12 -0700

    Task 5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,49 +1,101 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
 /**
- * is_digit - checks if a string contains a non-digit char
- * @s: string to be evaluated
+ * _print - moves a string one place to the left and prints the string
+ * @str: string to move
+ * @l: size of string
  *
- * Return: 0 if a non-digit is found, 1 otherwise
+ * Return: void
  */
-int is_digit(char *s)
+void _print(char *str, int l)
 {
-	int i = 0;
+	int i,  j;
 
-	while (s[i])
+	i = j = 0;
+	while (i < 1)
 	{
-		if (s[i] < '0' || s[i] > '9')
-			return (0);
-		i++
-	}
-	return (1);
-}
-
-/**
- * _strlen - returns the length of a string
- * @s: string to evaluate
- *
- * Return: the length of the string
- */
-int _strlen(char *s)
-{
-	int i = 0;
-
-	while (s[i] != '\0')
-	{
+		if (str[i] != '0')
+			j = 1;
+		if (j || i = l - 1)
+			_putchar(str[i]);
 		i++;
 	}
-	return (i);
+	_putchar('\n');
+	free(str);
 }
 
 /**
- * errors - handles errors for main
+ * mul - multiplies a character with a string and places the answer into dest
+ * @n: character to multiply
+ * @num: string to multiply
+ * @num_index: last non NULL index of num
+ * @dest: destination of the multiplication
+ * @dest_index: highest index to start addition
+ *
+ * Return: pointer to dest or NULL on failure
  */
-void errors(void)
+char *mul(char n, char *num, int num_index, char *dest, int dest_index)
 {
-	printf("Error\n");
-	exit(98);
+	int j, k, mul, mulrem, add, addrem;
+
+	mulrem = addrem = 0;
+
+	for (j = num_index, k = dest_index; j >= 0; j--, k--)
+	{
+		mul = (n - '0') * (num[j] - '0') + mulrem;
+		mulrem = mul / 10;
+		add = (dest[k] - '0') + (mul % 10) + addrem;
+		addrem = add / 10;
+		dest[k] = add % 10 + '0';
+	}
+	for (addrem += mulrem; k >= 0 && addrem; k--)
+	{
+		add = (dest[k] - '0') + addrem;
+		addrem = add / 10;
+		dest[k] = add % 10 + '0';
+	}
+	if (addrem)
+	{
+		return (NULL);
+	}
+	return (dest);
+}
+
+/**
+ * check_for_digits - checks the arguments to ensure they are digits
+ * @av: pointer to arguments
+ *
+ * Return: 0 if digits, 1 if not
+ */
+int check_for_digits(char **av)
+{
+	int i, j;
+
+	for (i = 1; i < 3; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			if (av[i][j] < '0' || av[i][j] > '9')
+				return (1);
+		}
+	}
+	return (0);
+}
+
+/**
+ * init - initializes a string
+ * @str: string to initialize
+ * @l: length of string
+ *
+ * Return: void
+ */
+void init(char *str, int l)
+{
+	int i;
+
+	for (i = 0; i < l; i++)
+		str[i] = '0';
+	str[i] = '\0';
 }
 
 /**
