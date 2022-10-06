f7c727f639b240503dcad3c67eb2b20492036029
KcMartins <kcmartinsikechukwu@yahoo.com>
2022-10-06 08:17:31 -0700

    Task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,15 +3,16 @@
 
 /**
  * *malloc_checked - allocates memory using malloc
- * @b: number of bytes to allocate
+ * @b: memory to allocate
  *
- * Return: a pointer to the allocated memory or normal process termination if error
+ * Return: pointer to allocated memory or normal process termination if error
  */
 void *malloc_checked(unsigned int b)
 {
 	void *ptr;
 
 	ptr = malloc(b);
+
 	if (ptr == NULL)
 		exit(98);
 	return (ptr);
