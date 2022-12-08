f90bc57a5c9df78e16fbd855a9c69479de632977
KcMartins <kcmartinsikechukwu@yahoo.com>
2022-12-08 05:09:22 -0800

    Task 11

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -1,110 +1,58 @@
+/*
+ * File: 103-keygen.c
+ * Auth: Brennan D Baraban
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "lists.h"
 
-/**
- * _strlen - returns the length of the string
- * @s: takes string
- *
- * Return: returns the length int
- */
-
-int _strlen(char *s)
-{
-	int count;
-
-	count = 0;
-	while (s[count])
-	{
-		count++;
-	}
-	return (count);
-}
-
-
-int f4(int len, char *argv)
-{
-	int i = 0, j = 0;
-
-	while (i < len)
-	{
-		if (argv[i] > j)
-			j = argv[i];
-		i++;
-	}
-	srand(j ^ 14);
-	i = rand();
-	return (i);
-}
-
-
-int f5(int len, char *argv)
-{
-	int i = 0;
-	long int j = 0;
-
-	while (i < len)
-	{
-		j += argv[i] * argv[i];
-		j = (int)j;
-		i++;
-	}
-	j = (j ^ 239) & 63;
-	return (j);
-}
-
-
-int f6(char *argv)
-{
-	int i = 0;
-	long int j = 0;
-
-	while (i < argv[0])
-	{
-		j = rand();
-		i++;
-	}
-	j = (j ^ 229) & 63;
-	return (j);
-
-}
 /**
  * main - Generates and prints passwords for the crackme5 executable.
- * @argc:
- * @argv:
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
  * Return: Always 0.
  */
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	int i = 0;
-	long int st = 0;
-	int len = 0;
-	char arr[7] = {0};
-	char t[] = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+	char password[7], *codex;
+	int len = strlen(argv[1]), i, tmp;
 
-	if (argc != 2)
-		return (-1);
-	len = _strlen(argv[1]);
-	arr[0] = t[(len ^ 59) & 63];
+	codex = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 
-	while (i < len)
+	tmp = (len ^ 59) & 63;
+	password[0] = codex[tmp];
+
+	tmp = 0;
+	for (i = 0; i < len; i++)
+		tmp += argv[1][i];
+	password[1] = codex[(tmp ^ 79) & 63];
+
+	tmp = 1;
+	for (i = 0; i < len; i++)
+		tmp *= argv[1][i];
+	password[2] = codex[(tmp ^ 85) & 63];
+
+	tmp = 0;
+	for (i = 0; i < len; i++)
 	{
-		st += argv[1][i];
-		i++;
+		if (argv[1][i] > tmp)
+			tmp = argv[1][i];
 	}
-	arr[1] = t[(st ^ 79) & 63];
-	st = 1, i = 0;
-	while (i < len)
-	{
-		st *= argv[1][i];
-		st = (int)st;
-		i++;
-	}
-	arr[2] = t[(st ^ 85) & 63];
-	arr[3] = t[f4(len, argv[1]) & 63];
-	arr[4] = t[f5(len, argv[1])];
-	arr[5] = t[f6(argv[1])];
-	printf("%s", arr);
+	srand(tmp ^ 14);
+	password[3] = codex[rand() & 63];
+
+	tmp = 0;
+	for (i = 0; i < len; i++)
+		tmp += (argv[1][i] * argv[1][i]);
+	password[4] = codex[(tmp ^ 239) & 63];
+
+	for (i = 0; i < argv[1][0]; i++)
+		tmp = rand();
+	password[5] = codex[(tmp ^ 229) & 63];
+
+	password[6] = '\0';
+	printf("%s", password);
 	return (0);
 }
